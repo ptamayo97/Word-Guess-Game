3484361f3e84c4fad51844491a553e234ceee240
ptamayo <ptamayo97@yahoo.com>
2018-08-26 15:39:33 -0700

    working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,82 +18,83 @@ function startGame() {
 
     computerChoice = wordToGuess[Math.floor(Math.random() * wordToGuess.length)];
     console.log(computerChoice);
+    letters = computerChoice.split("");
+    underScore = letters.length;
 
     guessesLeft = 9;
     wrongGuesses = [];
     wordProgress = [];
-    underScore = []
+    
 
 
     for (let i = 0; i < computerChoice.length; i++) {
-        underScore.push('_');
+        wordProgress.push('_');
     }
+
+    
     console.log(underScore)
+
     document.getElementById("word-blanks").innerHTML = wordProgress.join(" ");
     document.getElementById("guesses-left").innerHTML = guessesLeft;
     document.getElementById("win-counter").innerHTML = wins;
     document.getElementById("loss-counter").innerHTML = losses;
+}  
 
-
-
-    document.onkeyup = function (event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-
-        if (userGuess == computerChoice) {
-            document.getElementById("word-blanks").innerHTML = userGuess;
-        }
-
-
-        function checkGuess(guess) {
-            var isUserCorrect = false;
-            for (var i = 0; i < underScore; i++) {
-                if (computerChoice[i] == letter) {
-                    isUserCorrect = true;
+    function checkGuess(letter) {    
+        var isUserCorrect = false;
+        for (var i = 0; i < underScore; i++) {
+            if (computerChoice[i] == letter) {
+                isUserCorrect = true;
             }
         }
 
+        
         if (isUserCorrect) {
-
-
             for (var i = 0; i < underScore; i++) {
-                if (computerChoice[i] == guess) {
-                    wordProgress[i] = guess;
+                if (computerChoice[i] == letter) {
+                    wordProgress[i] = letter;
                 }
             }
         }
         else {
-            wrongGuesses.push(guess)
+            wrongGuesses.push(letter)
             guessesLeft--;
         }
-        
+    
     }
-    
-    
-    function userWin() {
-        console.log("win Count " + wins + " | Loss Count: " + losses + " | Guesses Left: " + guessesLeft)
 
-        document.getElementById("guesses-left").innerHTML = guessesLeft;
-        document.getElementById("word-blanks").innerHTML = wordProgress.join(" ");
-        document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-        if (letters.toString() == wordProgress.toString()) {
-            wins++;
-            alert("You Win!")
-            document.getElementById("win-counter").innerHTML = winCount;
-            startGame();
-        } else if (guessesLeft == 0) {
-            losses++;
-            alert("You Lose!");
-            document.getElementById("loss-counter")
-            startGame();
-        }
+function userWin() {
+    console.log("win Count " + wins + " | Loss Count: " + losses + " | Guesses Left: " + guessesLeft)
+
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("word-blanks").innerHTML = wordProgress.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+    if (letters.toString() == wordProgress.toString()) {
+        wins++;
+        alert("You Win!")
+        document.getElementById("win-counter").innerHTML = wins;
+        startGame();
+    } else if (guessesLeft == 0) {
+        losses++;
+        alert("You Lose!");
+        document.getElementById("loss-counter").innerHTML = losses;
+        startGame();
     }
-    
-
-
-
-
-
 }
+  
 startGame();
+
+document.onkeyup = function (event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    checkGuess(userGuess);
+    userWin();
+       
+
+        
+}
+
+
+
+
+
 
